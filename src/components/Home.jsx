5ac2c39de35f113de3ad25b2adf8d8597823a5fc
Youssef Salinas <lsalinasserrato@gmail.com>
2021-05-16 14:10:21 +0200

    Añadido el navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/scss/Home.scss';
 
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlus
 } from "@fortawesome/free-solid-svg-icons";
 
 export function Home() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="home">
       <h1>Recetas</h1>
@@ -15,6 +21,27 @@ export function Home() {
         <FontAwesomeIcon icon={faPlus} />
          Añadir nueva receta
       </Button>
+
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
     </div>
   )
 }
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,64 @@
+import React, { Fragment, useState } from "react";
+import "../assets/scss/Navbar.scss";
+
+import {
+  Button,
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+
+export function NavbarComponent() {
+  const [isConnected, setConnected] = useState(false);
+
+  function handleLogin() {
+    setConnected(true);
+  }
+
+  function handleLogout() {
+    setConnected(false);
+  }
+
+  return (
+    <Fragment>
+      <Navbar>
+        <Navbar.Brand>Recetas</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="mr-auto">
+            <NavDropdown title="Categorías">
+              <NavDropdown.Item href="#action/3.1">Desayunos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Comidas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Cenas</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Buscar alguna receta" className="mr-sm-2" />
+            <Button variant="outline-info">Buscar</Button>
+          </Form>
+          {!isConnected ? (
+            <Button 
+              className="log-in" 
+              variant="success" 
+              onClick={handleLogin}>Iniciar sesión
+            </Button>
+          ) : (
+            <NavDropdown title="Mi cuenta" className="account">
+              <NavDropdown.Item href="#action/3.1">Mis datos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Mis recetas</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <Button 
+                className="log-out" 
+                variant="danger" 
+                onClick={handleLogout}>Cerrar sesión
+              </Button>
+            </NavDropdown>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    </Fragment>
+  );
+}
