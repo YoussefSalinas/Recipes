b081a0534e2fbb6ff8c2d376c82a91631495510a
Youssef Salinas <lsalinasserrato@gmail.com>
2021-05-23 22:08:03 +0200

    Agregada las transiciones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ import { RecetaList } from './RecetaList';
 
 import Carousel from 'react-elastic-carousel';
 import { Card } from './Card';
+import { CardCarousel } from './CardCarousel';
 
 export function Home() {
   const [recetas, setRecetas] = useState([
@@ -96,90 +97,90 @@ export function Home() {
       nombreUsuario: 'Ana',
       imagenUsuario: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
     },
-    {
-      id: 4, 
-      nombre: 'Desayuno 1', 
-      categoria: 'desayunos',
-      descripcion: 'Descripción del platillo',
-      fechaCreacion: '16/05/2021',
-      dificultad: 'facil',
-      porciones: 2,
-      tiempoPreparacion: 0,
-      tiempoCoccion: 30,
-      tiempoDescanso: 0,
-      utensilios: [
-        { id: 1, cantidad: 1, utensilio: "Tabla de picar" },
-        { id: 2, cantidad: 1, utensilio: "Parrilla"}
-      ],
-      ingredientes: [
-        { cantidad: 1, ingrediente: "Salsa de Mamá Mexicana", tipo: "" },
-        { cantidad: 2, ingrediente: "filetes de media pechuga de pollo sin piel y sin hueso", tipo: ""},
-        { cantidad: 1, ingrediente: "sazonador para parrilleras", tipo: "cdita"}
-      ],
-      preparacion: [
-        { numeroPaso: 1, paso: "Corta en cuadrados medianos los filetes de pechuga." },
-        { numeroPaso: 2, paso: "Lava el pimiento y córtalos en cuadrados medianos."}
-      ],
-      imagen: 'https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
-      nombreUsuario: 'Ana',
-      imagenUsuario: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-    },
-    {
-      id: 5,
-      nombre: 'Comida 1',
-      categoria: 'comidas',
-      descripcion: 'Descripción del platillo',
-      fechaCreacion: '16/05/2021',
-      dificultad: 'facil',
-      porciones: 2,
-      tiempoPreparacion: 0,
-      tiempoCoccion: 30,
-      tiempoDescanso: 0,
-      utensilios: [
-        { id: 1, cantidad: 1, utensilio: "Tabla de picar" },
-        { id: 2, cantidad: 1, utensilio: "Parrilla"}
-      ],
-      ingredientes: [
-        { cantidad: 1, ingrediente: "Salsa de Mamá Mexicana", tipo: "" },
-        { cantidad: 2, ingrediente: "filetes de media pechuga de pollo sin piel y sin hueso", tipo: ""},
-        { cantidad: 1, ingrediente: "sazonador para parrilleras", tipo: "cdita"}
-      ],
-      preparacion: [
-        { numeroPaso: 1, paso: "Corta en cuadrados medianos los filetes de pechuga." },
-        { numeroPaso: 2, paso: "Lava el pimiento y córtalos en cuadrados medianos."}
-      ],
-      imagen: 'https://images.unsplash.com/photo-1428259067396-2d6bd3827878?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80',
-      nombreUsuario: 'Youssef',
-      imagenUsuario: 'https://uifaces.co/our-content/donated/NY9hnAbp.jpg'
-    },
-    {
-      id: 6,
-      nombre: 'Cena 1',
-      categoria: 'cenas',
-      descripcion: 'Descripción del platillo',
-      fechaCreacion: '16/05/2021',
-      dificultad: 'medio',
-      porciones: 2,
-      tiempoPreparacion: 0,
-      tiempoCoccion: 30,
-      tiempoDescanso: 0,
-      utensilios: [
-        { id: 1, cantidad: 1, utensilio: "Tabla de picar" },
-        { id: 2, cantidad: 1, utensilio: "Parrilla"}
-      ],
-      ingredientes: [
-        { cantidad: 1, ingrediente: "Salsa de Mamá Mexicana", tipo: "" },
-        { cantidad: 2, ingrediente: "filetes de media pechuga de pollo sin piel y sin hueso", tipo: ""},
-        { cantidad: 1, ingrediente: "sazonador para parrilleras", tipo: "cdita"}
-      ],
-      preparacion: [
-        { numeroPaso: 1, paso: "Corta en cuadrados medianos los filetes de pechuga." },
-        { numeroPaso: 2, paso: "Lava el pimiento y córtalos en cuadrados medianos."}
-      ],
-      imagen: 'https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=685&q=80',
-      nombreUsuario: 'Ana',
-      imagenUsuario: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-    }
+    // {
+    //   id: 4, 
+    //   nombre: 'Desayuno 1', 
+    //   categoria: 'desayunos',
+    //   descripcion: 'Descripción del platillo',
+    //   fechaCreacion: '16/05/2021',
+    //   dificultad: 'facil',
+    //   porciones: 2,
+    //   tiempoPreparacion: 0,
+    //   tiempoCoccion: 30,
+    //   tiempoDescanso: 0,
+    //   utensilios: [
+    //     { id: 1, cantidad: 1, utensilio: "Tabla de picar" },
+    //     { id: 2, cantidad: 1, utensilio: "Parrilla"}
+    //   ],
+    //   ingredientes: [
+    //     { cantidad: 1, ingrediente: "Salsa de Mamá Mexicana", tipo: "" },
+    //     { cantidad: 2, ingrediente: "filetes de media pechuga de pollo sin piel y sin hueso", tipo: ""},
+    //     { cantidad: 1, ingrediente: "sazonador para parrilleras", tipo: "cdita"}
+    //   ],
+    //   preparacion: [
+    //     { numeroPaso: 1, paso: "Corta en cuadrados medianos los filetes de pechuga." },
+    //     { numeroPaso: 2, paso: "Lava el pimiento y córtalos en cuadrados medianos."}
+    //   ],
+    //   imagen: 'https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    //   nombreUsuario: 'Ana',
+    //   imagenUsuario: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+    // },
+    // {
+    //   id: 5,
+    //   nombre: 'Comida 1',
+    //   categoria: 'comidas',
+    //   descripcion: 'Descripción del platillo',
+    //   fechaCreacion: '16/05/2021',
+    //   dificultad: 'facil',
+    //   porciones: 2,
+    //   tiempoPreparacion: 0,
+    //   tiempoCoccion: 30,
+    //   tiempoDescanso: 0,
+    //   utensilios: [
+    //     { id: 1, cantidad: 1, utensilio: "Tabla de picar" },
+    //     { id: 2, cantidad: 1, utensilio: "Parrilla"}
+    //   ],
+    //   ingredientes: [
+    //     { cantidad: 1, ingrediente: "Salsa de Mamá Mexicana", tipo: "" },
+    //     { cantidad: 2, ingrediente: "filetes de media pechuga de pollo sin piel y sin hueso", tipo: ""},
+    //     { cantidad: 1, ingrediente: "sazonador para parrilleras", tipo: "cdita"}
+    //   ],
+    //   preparacion: [
+    //     { numeroPaso: 1, paso: "Corta en cuadrados medianos los filetes de pechuga." },
+    //     { numeroPaso: 2, paso: "Lava el pimiento y córtalos en cuadrados medianos."}
+    //   ],
+    //   imagen: 'https://images.unsplash.com/photo-1428259067396-2d6bd3827878?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80',
+    //   nombreUsuario: 'Youssef',
+    //   imagenUsuario: 'https://uifaces.co/our-content/donated/NY9hnAbp.jpg'
+    // },
+    // {
+    //   id: 6,
+    //   nombre: 'Cena 1',
+    //   categoria: 'cenas',
+    //   descripcion: 'Descripción del platillo',
+    //   fechaCreacion: '16/05/2021',
+    //   dificultad: 'medio',
+    //   porciones: 2,
+    //   tiempoPreparacion: 0,
+    //   tiempoCoccion: 30,
+    //   tiempoDescanso: 0,
+    //   utensilios: [
+    //     { id: 1, cantidad: 1, utensilio: "Tabla de picar" },
+    //     { id: 2, cantidad: 1, utensilio: "Parrilla"}
+    //   ],
+    //   ingredientes: [
+    //     { cantidad: 1, ingrediente: "Salsa de Mamá Mexicana", tipo: "" },
+    //     { cantidad: 2, ingrediente: "filetes de media pechuga de pollo sin piel y sin hueso", tipo: ""},
+    //     { cantidad: 1, ingrediente: "sazonador para parrilleras", tipo: "cdita"}
+    //   ],
+    //   preparacion: [
+    //     { numeroPaso: 1, paso: "Corta en cuadrados medianos los filetes de pechuga." },
+    //     { numeroPaso: 2, paso: "Lava el pimiento y córtalos en cuadrados medianos."}
+    //   ],
+    //   imagen: 'https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=685&q=80',
+    //   nombreUsuario: 'Ana',
+    //   imagenUsuario: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+    // }
   ]);
 
   const [index, setIndex] = useState(0);
@@ -213,6 +214,7 @@ export function Home() {
           <Card className="carta" number="6" />
         </Carousel>
       </Container>
+      <CardCarousel />
     </div>
   )
 }
--- src/components/RecetaList.jsx
+++ src/components/RecetaList.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import '../assets/scss/RecetaList.scss';
 
-import { Container, CardDeck, CardGroup, CardColumns, Row, Carousel } from 'react-bootstrap';
+import { Container, CardDeck, CardGroup, CardColumns, Row } from 'react-bootstrap';
 
 import { RecetaDetalle } from './RecetaDetalle'
 
+import Carousel from 'react-elastic-carousel';
+
 export function RecetaList({ recetas }) {
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 500, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 }
+  ]
+
   return (
     <Container>
       <CardGroup>
         <h2 className="title">Nuevas recetas</h2>
+          <Carousel breakPoints={breakPoints}>
         <Row>
-          {recetas.map(( receta ) => (
-            <RecetaDetalle key={receta.id} receta={receta} />
-          ))}
+            {recetas.map(( receta, index ) => (
+              <RecetaDetalle 
+                key={receta.id} 
+                receta={receta} 
+              />
+            ))}
         </Row>
+          </Carousel>
       </CardGroup>
     </Container>
   )
